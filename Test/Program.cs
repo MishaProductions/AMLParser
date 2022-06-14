24e2d020ce34360e0d5d7e0222ab2f20ac751e0b
valentinbreiz <valentinbreiz@gmail.com>
2022-06-14 18:06:25 +0200

    Add test for testing AML without Cosmos

---
+++ Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CosmosACPIAMl
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+
+            Console.ReadKey();
+        }
+    }
+}
